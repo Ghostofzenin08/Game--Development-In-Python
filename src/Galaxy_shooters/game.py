ec1263d0c956aaf7ed311881c931970f40c3c1d6
Ghostofzenin08 <harshalsonawane308@gmail.com>
2026-08-19 21:41:14 +0530

    Add keys

--- src/Galaxy_shooters/game.py
+++ src/Galaxy_shooters/game.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 
+ASSET_DIR = os.path.join(os.path.dirname(__file__), 'assets', 'images')
 
 
 
@@ -12,20 +13,61 @@ pygame.display.set_caption("Galaxy Shooters")
 WHITE = (255, 255, 255)
 
 FPS = 60
-
-YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('pass'))
-RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('pass'))
+VEL = 5
 
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join(ASSET_DIR, 'yellow_spaceship.png'))
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join(ASSET_DIR, 'red_spaceship.png'))
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window():
+
+
+def draw_window(red, yellow):
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP_IMAGE, (red.x, red.y))
 
-    pygame.display.update
+    pygame.display.update()
+
+
+def yellow_handle_movement(keys_pressed, yellow):
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_a]:  # LEFT 
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d]:  # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_w]:  # UP
+        yellow.y -= VEL
+    if keys_pressed[pygame.K_s]:  # DOWN
+        yellow.y += VEL
+
+
+def red_handle_movement(keys_pressed, red):
+    keys_pressed = pygame.key.get_pressed()
+    if keys_pressed[pygame.K_a]:  # LEFT 
+        red.x -= VEL
+    if keys_pressed[pygame.K_d]:  # RIGHT
+        red.x += VEL
+    if keys_pressed[pygame.K_w]:  # UP
+        red.y -= VEL
+    if keys_pressed[pygame.K_s]:  # DOWN
+        red.y += VEL
+
+
+
+
 
 def main():
+    red = pygame.Rect(100, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(700, 300, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -34,7 +76,12 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window()    
+
+
+
+        draw_window(red, yellow)
+
+
 
     pygame.quit()
 
